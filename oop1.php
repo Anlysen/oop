e92f40b110a2151b4e73245c113156c8c1e3c248
Andrew <rostangeles1993@gmail.com>
2023-07-19 16:41:56 +0300

    New commit

---
+++ oop1.php
@@ -0,0 +1,16 @@
+<?php
+
+class ShoppingCart {
+
+}
+
+$product1 = new ShoppingCart();
+$product2 = new ShoppingCart();
+$product3 = new ShoppingCart();
+
+var_dump($product1 instanceof ShoppingCart);
+var_dump($product2 instanceof ShoppingCart);
+var_dump($product3 instanceof Shopping);
+
+
+?>
